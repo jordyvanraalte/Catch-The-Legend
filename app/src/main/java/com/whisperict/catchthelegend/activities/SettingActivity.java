77b1fefb6bf208d3042bd727bd2457bde8196ac0
Unknown <daningteen@gmail.com>
2018-12-10 00:10:25 +0100

    [Added] Small Changes Settings

--- app/src/main/java/com/whisperict/catchthelegend/activities/SettingActivity.java
+++ app/src/main/java/com/whisperict/catchthelegend/activities/SettingActivity.java
@@ -4,12 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.whisperict.catchthelegend.R;
 
 public class SettingActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
+
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
