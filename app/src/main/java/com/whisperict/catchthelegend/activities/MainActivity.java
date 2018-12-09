7381a072994c75b5f7fc03d3bd3928d7f7853c62
Unknown <daningteen@gmail.com>
2018-12-09 15:12:20 +0100

    [Added] Style Things Navigation Drawer

--- app/src/main/java/com/whisperict/catchthelegend/activities/MainActivity.java
+++ app/src/main/java/com/whisperict/catchthelegend/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.whisperict.catchthelegend;
+package com.whisperict.catchthelegend.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.whisperict.catchthelegend.R;
 
 public class MainActivity extends AppCompatActivity {
 
