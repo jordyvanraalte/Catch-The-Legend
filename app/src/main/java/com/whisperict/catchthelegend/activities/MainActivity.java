9efb66a634a6b92f0bb62147fe7608b7f4e4a7eb
Unknown <daningteen@gmail.com>
2018-12-09 23:40:52 +0100

    [Added] Click Events and Settings Activity

--- app/src/main/java/com/whisperict/catchthelegend/activities/MainActivity.java
+++ app/src/main/java/com/whisperict/catchthelegend/activities/MainActivity.java
@@ -1,32 +1,78 @@
 package com.whisperict.catchthelegend.activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.whisperict.catchthelegend.R;
+import com.whisperict.catchthelegend.fragments.CompendiumFragment;
+import com.whisperict.catchthelegend.fragments.MapFragment;
+import com.whisperict.catchthelegend.fragments.QuestFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView =  findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new MapFragment()).commit();
+            navigationView.setCheckedItem(R.id.map);
+            toolbar.setTitle("Map");
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.map:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MapFragment()).commit();
+                toolbar.setTitle("Map");
+                break;
+            case R.id.quest:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new QuestFragment()).commit();
+                toolbar.setTitle("Quest");
+                break;
+            case R.id.compendium:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CompendiumFragment()).commit();
+                toolbar.setTitle("Compendium");
+                break;
+
+            case R.id.settings:
+                Intent intent = new Intent (MainActivity.this, SettingActivity.class);
+                //todo putExtra's?
+                MainActivity.this.startActivity(intent);
+                toolbar.setTitle("Settings");
+                break;
+        }
+        return true;
     }
 
     @Override
