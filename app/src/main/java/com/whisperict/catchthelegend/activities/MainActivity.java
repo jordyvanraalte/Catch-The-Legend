a704dd854493d334f8613a41ae045faa432da283
Unknown <daningteen@gmail.com>
2018-12-11 14:01:37 +0100

    [Added] Strings and Help Dialog Fragment

--- app/src/main/java/com/whisperict/catchthelegend/activities/MainActivity.java
+++ app/src/main/java/com/whisperict/catchthelegend/activities/MainActivity.java
@@ -3,16 +3,19 @@ package com.whisperict.catchthelegend.activities;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.whisperict.catchthelegend.R;
 import com.whisperict.catchthelegend.fragments.CompendiumFragment;
+import com.whisperict.catchthelegend.fragments.HelpDialogFragment;
 import com.whisperict.catchthelegend.fragments.MapFragment;
 import com.whisperict.catchthelegend.fragments.QuestFragment;
 
@@ -42,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new MapFragment()).commit();
             navigationView.setCheckedItem(R.id.map);
-            toolbar.setTitle("Map");
+            toolbar.setTitle(R.string.map);
         }
     }
 
@@ -52,27 +55,46 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.map:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new MapFragment()).commit();
-                toolbar.setTitle("Map");
+                toolbar.setTitle(R.string.map);
                 break;
             case R.id.quest:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new QuestFragment()).commit();
-                toolbar.setTitle("Quest");
+                toolbar.setTitle(R.string.quests);
                 break;
             case R.id.compendium:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new CompendiumFragment()).commit();
-                toolbar.setTitle("Compendium");
+                toolbar.setTitle(R.string.compendium);
                 break;
 
             case R.id.settings:
                 Intent intent = new Intent (MainActivity.this, SettingActivity.class);
                 //todo putExtra's?
                 MainActivity.this.startActivity(intent);
-                toolbar.setTitle("Settings");
                 break;
         }
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+
+            case R.id.help:
+                DialogFragment dialog = new HelpDialogFragment();
+                dialog.show(getSupportFragmentManager(), "help");
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
---
+++ app/src/main/java/com/whisperict/catchthelegend/fragments/HelpDialogFragment.java
@@ -0,0 +1,6 @@
+package com.whisperict.catchthelegend.fragments;
+
+import android.support.v4.app.DialogFragment;
+
+public class HelpDialogFragment extends DialogFragment {
+}
