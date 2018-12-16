2526629a66e44bad3a5ceeac346d608e4b31c906
Unknown <daningteen@gmail.com>
2018-12-17 00:51:11 +0100

    [Added]Location

--- app/src/main/java/com/whisperict/catchthelegend/fragments/MapFragment.java
+++ app/src/main/java/com/whisperict/catchthelegend/fragments/MapFragment.java
@@ -1,12 +1,22 @@
 package com.whisperict.catchthelegend.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,38 +26,43 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.whisperict.catchthelegend.R;
+import com.whisperict.catchthelegend.activities.MainActivity;
 import com.whisperict.catchthelegend.managers.MapManager;
 
 public class MapFragment extends SupportMapFragment implements MapManager.OnMapReadyListener {
 
     private GoogleMap map;
+    private static final int REQUEST_PERMISSION_ID = 1;
+    private android.location.Location location;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        new MapManager(this, this);
+        new MapManager(this, this, getActivity());
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
-        LatLngBounds breda = new LatLngBounds.Builder()
-                .include(new LatLng(51.615751, 4.722507))
-                .include(new LatLng(51.606789, 4.807834))
-                .include(new LatLng(51.589740, 4.823363))
-                .include(new LatLng(51.521217, 4.771453))
-                .include(new LatLng(51.521168, 4.746086))
-                .include(new LatLng(51.574364, 4.691582))
-                .build();
-
-        map.setMinZoomPreference(14);
-        map.setMaxZoomPreference(20);
+        if (!checkPermission(Manifest.permission.ACCESS_FINE_LOCATION))
+            requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSION_ID);
+        else
+            map.setMyLocationEnabled(true);
 
         map.setBuildingsEnabled(false);
         map.setIndoorEnabled(false);
-
-        map.setLatLngBoundsForCameraTarget(breda);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(breda.getCenter(), 16));
     }
+
+    private boolean checkPermission(String permission) {
+        if (ActivityCompat.checkSelfPermission(getContext(), permission)
+                == PackageManager.PERMISSION_GRANTED)
+            return true;
+        return false;
+    }
+
+    private void requestPermission(String permission, int code) {
+        requestPermissions(new String[]{permission}, code);
+    }
+    
 }
--- app/src/main/java/com/whisperict/catchthelegend/managers/MapManager.java
+++ app/src/main/java/com/whisperict/catchthelegend/managers/MapManager.java
@@ -1,5 +1,12 @@
 package com.whisperict.catchthelegend.managers;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 
@@ -9,17 +16,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapManager implements ViewTreeObserver.OnGlobalLayoutListener, OnMapReadyCallback {
 
+    private static final String TAG = "Permission";
     private SupportMapFragment mapFragment;
     private OnMapReadyListener listener;
+    private Context context;
 
     private GoogleMap googleMap;
     private boolean isMapReady = false;
     private boolean isViewReady = false;
 
-    public MapManager(SupportMapFragment fragment, OnMapReadyListener listener) {
+    public MapManager(SupportMapFragment fragment, OnMapReadyListener listener, Context context) {
         this.mapFragment = fragment;
         this.listener = listener;
-
+        this.context = context;
         registerListeners();
     }
 
