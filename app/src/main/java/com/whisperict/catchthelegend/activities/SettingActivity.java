dafe2d2bd26d4270a9a12a38b235b6c7e69a152b
Unknown <daningteen@gmail.com>
2018-12-19 21:35:36 +0100

    [Fixed] Settings Screen

--- app/src/main/java/com/whisperict/catchthelegend/activities/SettingActivity.java
+++ app/src/main/java/com/whisperict/catchthelegend/activities/SettingActivity.java
@@ -3,10 +3,15 @@ package com.whisperict.catchthelegend.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.whisperict.catchthelegend.R;
+
+import java.util.Objects;
 
 public class SettingActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -19,7 +24,12 @@ public class SettingActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(R.string.system_settings);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.system_settings);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.primary_dark));
     }
 }
